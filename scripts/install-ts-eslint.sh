a89351ebf1a6fe6470e5858cb15f3ca8c6bd8bce
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-09-19 10:50:07 -0400

    chore(home-react): :rotating_light: Add linting rules update dependencies.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- scripts/install-ts-eslint.sh
+++ scripts/install-ts-eslint.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 pwd
-pnpm i -D @types/node@latest @types/eslint-config-prettier jest nodemon prettier ts-jest ts-node typescript \
+pnpm i -D \
+  @types/eslint-config-prettier \
   @types/eslint-plugin-prettier \
+  @types/jest \
+  @types/node@latest \
   @typescript-eslint/eslint-plugin \
   @typescript-eslint/parser \
   eslint \
   eslint-config-prettier \
+  eslint-import-resolver-typescript \
   eslint-plugin-import \
   eslint-plugin-jsdoc \
   eslint-plugin-jsonc \
   eslint-plugin-prettier \
+  eslint-plugin-react \
+  eslint-plugin-react-hooks \
   eslint-plugin-simple-import-sort \
   eslint-plugin-unicorn \
-  eslint-import-resolver-typescript
+  jest \
+  nodemon \
+  prettier \
+  ts-jest \
+  ts-node \
+  typescript
