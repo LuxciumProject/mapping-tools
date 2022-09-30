aec501d996f661d64b995d091c1dbc4b6fc3048c
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-09-30 03:14:42 -0400

    feat(servers): :construction: Add basic files to project.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

---
+++ servers/scratch/src/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+void (async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(3000);
+})();
