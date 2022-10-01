00f135eb927ca740327bb2ca78ae9bef62f8d5ae
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-01 12:52:02 -0400

    build(api-server): :whale: Dockerize deployment.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- servers/api/src/main.ts
+++ servers/api/src/main.ts
@@ -2,9 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { HomeModule } from './home/home.module';
 
-async function bootstrap() {
+void (async function bootstrap() {
   const app = await NestFactory.create(HomeModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
-}
-bootstrap();
+  await app.listen(80);
+})();
