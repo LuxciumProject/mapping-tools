bbb65781e6f281a5ad6a45ac15912a3dca15b15c
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-08 00:57:29 -0400

    refactor(home-react): :ambulance: Remove scss.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- clients/home2/src/App.tsx
+++ clients/home2/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import AddUser from './components/Users/AddUser';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AddUser />
       </header>
     </div>
   );
 }
 
 export default App;
+  // const [fetchedData, setFetchedData] = useState([] as any[]);
+
+  // function fetchDataHandler() {
+  //   fetch(`${hostName}api`)
+  //     .then(response => response.text())
+  //     .then(data => setFetchedData([data]));
+  // }
+
+        // {/* <ColorCard property="editorError.border" hex="#FFFFFF" />
+        // <button onClick={fetchDataHandler}>Test Fetch</button>
+        // <ul>
+          // {fetchedData.map((data, index) => (
+            // <li key={index}> {data} </li>
+          // ))}
+        // </ul>{' '} */}
---
+++ clients/home2/src/components/ColorCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import css from './ColorCard.module.css';
+export const ColorCard = (props: any) => {
+  return (
+    <div className={css.card}>
+      <div className={css.container}>
+        <h4 style={{ background: props.hex }}>
+          <b>{props.hex}</b>
+        </h4>
+        <p>{props.property}</p>
+      </div>
+    </div>
+  );
+};
+export default ColorCard;
