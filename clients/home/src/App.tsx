eadfa7f936ac4cd263288ed17fb19c644c73f2ba
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-08 00:54:45 -0400

    feat(home-react): :sparkles: Academind tutorial with scss.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- clients/home/src/App.tsx
+++ clients/home/src/App.tsx
@@ -1,25 +1,31 @@
-import './App.css';
-
 import React from 'react';
+// import ColorCard from './components/ColorCard';
+import AddUser from './components/Users/AddUser';
 
-import logo from './logo.svg';
+import './App.scss';
+
+// const hostName = '';
 
 function App() {
+  // const [fetchedData, setFetchedData] = useState([] as any[]);
+
+  // function fetchDataHandler() {
+  //   fetch(`${hostName}api`)
+  //     .then(response => response.text())
+  //     .then(data => setFetchedData([data]));
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <ColorCard property="editorError.border" hex="#FFFFFF" />
+        <button onClick={fetchDataHandler}>Test Fetch</button>
+        <ul>
+          {fetchedData.map((data, index) => (
+            <li key={index}> {data} </li>
+          ))}
+        </ul>{' '} */}
+        <AddUser />
       </header>
     </div>
   );
