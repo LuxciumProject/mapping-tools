2f02ca4f3e3af67953cb8586e14da55aa1316002
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-14 21:39:56 -0400

    chore: :see_no_evil: Ignore build folders.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- services/service-one/build/index.js
+++ services/service-one/build/index.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.createRedisClient = void 0;
 exports.default = () => console.log('Hello from service one');
+var redis_services_1 = require("@luxcium/redis-services");
+Object.defineProperty(exports, "createRedisClient", { enumerable: true, get: function () { return redis_services_1.createRedisClient; } });
