b1131ecf9d52ddf7d8907d5443cc90ece988ed5c
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-15 14:28:47 -0400

    chore(services): :building_construction: Upgrade rushStack toolings.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- services/phash-convertor/scripts/build.sh
+++ services/phash-convertor/scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tsc --noEmit false --generateCpuProfile \"./out/phash-convertor.cpuprofile\" || exit 1
+tsc --noEmit false --generateCpuProfile ./out/phash-convertor.cpuprofile || exit 1
