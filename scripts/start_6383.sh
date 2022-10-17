57e67d9d49621c8ac30157d2c9f5170b8b3ee5f8
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-17 08:47:04 -0400

    feat: :rocket: Add image-scout docker global (in repo) support.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- scripts/start_6383.sh
+++ scripts/start_6383.sh
@@ -2,9 +2,10 @@
 
 REDIS_6383_PING=$(redis-cli -p 6383 PING)
 if [ "$REDIS_6383_PING" != 'PONG' ]; then
-    /usr/src/Redis-ImageScout/docker_run_redis
+    services/image-scout/docker_run_redis
+else
+    echo 'already runing an instance of redis-cli <port=6383>'
 fi
-echo 'already runing an instance of redis-cli <port=6383>'
 sleep 2
 echo 'Will now monitor:'
 redis-cli -p 6383 monitor
