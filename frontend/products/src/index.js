e6eb7bc54c8813248552a1a65cf18a7d0ffa1a2b
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-10-18 10:12:09 -0400

    feat(frontend): :beers: Tutorial on Microfrontends.
    Microfrontends with React: A Complete Developer's Guide
    
    https://www.udemy.com/course/microfrontend-course/?kw=Microfrontends+with+React%3A+A+Complete+Developer%27s+Guide
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

---
+++ frontend/products/src/index.js
@@ -0,0 +1,11 @@
+import faker from 'faker';
+
+let products = '';
+
+for (let i = 0; i < 10; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+}
+
+
+console.log(products);
