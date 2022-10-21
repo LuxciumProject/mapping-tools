28812cecd78445e4c49624e7edb485b21548101f
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-10-21 10:36:09 -0400

    feat(frontend): :sparkles: Add ModuleFederationPlugin.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- frontend/federal-container/webpack.config.cjs
+++ frontend/federal-container/webpack.config.cjs
@@ -1,21 +1,19 @@
 const HtmlWebpackPluggin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/Module/FederationPlugin');
+
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development', // or production
   plugins: [new BundleAnalyzerPlugin()],
   devServer: {
-    port: 8081,
+    port: 8080,
   },
 
   plugins: [
     new ModuleFederationPlugin({
-      name: 'products',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './productsIndex': './src/index',
-      },
+      name: 'container',
+      remotes: { products: 'products@http://localhost:8081/remoteEntry.js' },
     }),
     new HtmlWebpackPluggin({
       template: './public/index.html',
