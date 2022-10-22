0269e673fbc49380f86f2e7c7c3cde70d8e1760b
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-10-22 09:49:41 -0400

    feat(frontend): :children_crossing: Following a tutorial...
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

---
+++ frontend/products/src/bootstrap.js
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+
+let products = '';
+
+for (let i = 0; i < 10; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+}
+const devProducts = document.querySelector('#dev-products');
+
+devProducts ? (devProducts.innerHTML = products) : void null;
+
+console.log('From Products Page !!!');
