a81ba25d8b8c99122013b2168932d84335300f53
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-10-24 05:45:56 -0400

    feat(backend): :beers: Try something.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- backend/api/src/trySomething.ts
+++ backend/api/src/trySomething.ts
@@ -1,15 +1,23 @@
 import getPhashCompute from '@luxcium/phash-compute';
 import { rConnect } from '@luxcium/redis-services';
+import { from } from '@luxcium/scan-dirs';
 rConnect;
 getPhashCompute;
 
 void (async function main() {
   const R = await rConnect(6383);
   const phashCompute = getPhashCompute(R);
-  console.log(
-    await phashCompute(
-      '/home/luxcium/projects/monorepo-one/public/imgs/unsplash-Toby Wong.jpg',
-    ),
-  );
-  return phashCompute;
+  const scaner = from('/home/luxcium/projects/monorepo-one/public/imgs/')
+    .addValidExt(['.jpg', '.png'])
+    .map(phashCompute);
+
+  for await (const some of scaner) {
+    console.log(some);
+  }
+  // console.log(
+  //   await phashCompute(
+  //     '/home/luxcium/projects/monorepo-one/public/imgs/unsplash-Toby Wong.jpg',
+  //   ),
+  // );
+  // return phashCompute;
 })();
