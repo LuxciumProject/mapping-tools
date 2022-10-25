686f96877e82d15597ea5d3e63352df8241d5326
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-10-24 21:40:02 -0400

    chore(common): :bento: Updating stuff Generic commit.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- library/tools/src/typing-lessons/index.ts
+++ library/tools/src/typing-lessons/index.ts
@@ -1,4 +1,33 @@
 
+import * as tf from '@tensorflow/tfjs-node-gpu';
+
+tf.tensor
+// tf.scalar
+// tf.tensor1d
+// tf.tensor2d
+// tf.tensor3d
+// tf.tensor4d
+// tf.tensor5d
+// tf.tensor6d
+// tf.buffer
+// tf.clone
+// tf.complex
+// tf.diag
+// tf.eye
+// tf.fill
+// tf.imag
+// tf.linspace
+// tf.oneHot
+// tf.ones
+// tf.onesLike
+// tf.print
+// tf.range
+// tf.real
+// tf.truncatedNormal
+// tf.variable
+// tf.zeros
+// tf.zerosLike
+
 
 export const letterLists = [
   [
