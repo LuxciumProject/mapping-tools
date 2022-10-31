52021bf1ff575cf16afff8945cade9868b1d76cf
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-10-31 07:41:44 -0400

    docs(services): :bulb: Add and update comments in source code.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- services/phash-compute/src/getCachedPhashString_core.ts
+++ services/phash-compute/src/getCachedPhashString_core.ts
@@ -7,30 +7,43 @@ import { redisSetK } from './redisSetK';
 
 export const debug = false;
 debug && console.error('debug on in ', __filename);
+
 export function getCachedPhashString_core(R: RedisClientType | null = null) {
-  return async (filePath: string) => {
+  return async (compatibleImagefilePath: string) => {
     const pathObj = {
-      path: filePath,
-      size: (await promises.stat(filePath)).size,
+      path: compatibleImagefilePath,
+      size: (await promises.stat(compatibleImagefilePath)).size,
     };
 
+    // Without RedisClient no need to look into the cache
     if (!R) {
       const bigStr = await getBigStrPHashFromFile(pathObj.path);
       return bigStr;
     }
+
+    // Lookup if already inside the cache
     const lookUP = await pHashGetLookUp(R, pathObj.path);
+
+    // if LookUP is positive (not false and not emptystring)
     if (lookUP !== false && lookUP !== '') {
       debug && console.warn(`lookUP: ${lookUP}`);
+      // Then return from cache
       return lookUP;
     } else {
+      // Else calculate the value
       const bigStr = await getBigStrPHashFromFile(pathObj.path);
+
+      // If the calue is not the empty string
       if (bigStr !== '') {
+        // And if we can set it into the cache
         if (await redisSetK(R, pathObj.path, bigStr)) {
           debug && console.warn(`computed: ${bigStr}`);
+          // Return the newly calculated value
           return bigStr;
         }
       }
       debug && console.error(`Error: '<empty string>'`, pathObj);
+      // Return the newly calculated value
       return bigStr;
     }
   };
