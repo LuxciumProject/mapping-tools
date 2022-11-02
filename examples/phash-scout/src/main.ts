2770847110597b514911d0ca01ee4906dfea6af4
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-11-01 23:55:49 -0400

    refactor(examples): :zap: Improve phscout examples
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- examples/phash-scout/src/main.ts
+++ examples/phash-scout/src/main.ts
@@ -1,35 +1,73 @@
 // import { BoxedGenerator } from '@luxcium/boxed-list';
-import { getPhashCompute } from '@luxcium/phash-compute';
+// import { BoxedAsyncGenerator } from '@luxcium/boxed-list';
+import { boxImageFileWithStats, getPhashCompute } from '@luxcium/phash-compute';
 import { rConnect } from '@luxcium/redis-services';
 import { scanDirsFrom } from '@luxcium/scan-dirs';
-import { readFileSync } from 'fs';
+import { readFileSync, Stats } from 'node:fs';
 
 const configs = JSON.parse(readFileSync('/home/luxcium/projects/configs/mono-repo.jsonc').toString());
 
 void (async function main() {
   //
-  // Open the connection to the redis cache server
+  // -! Open the connection to the redis cache server
+
   const R = await rConnect(6383);
 
-  // Link the connection with the pHashCompute
-  const phashCompute = getPhashCompute(R, 'DEBUG');
+  // -! Link the connection with the pHashCompute
 
-  // Initialise the file scaner to scan each folders from the link provided
+  const phashCompute = getPhashCompute(R, false);
+
+  // -! Initialise the file scaner to scan each folders from the link provided
+
   const folders = scanDirsFrom(configs['examples-phash-scout'].links.example);
 
-  // Then set the valid files extensions to be used from the file scanner
+  // -! Then set the valid files extensions to be used from the file scanner
+
   folders.addValidExt(['.jpg', '.png']);
-  // const scaner = BoxedGenerator.fromGen<any>(folders.scan);
 
-  // Use the pHashCompute on each filesPaths with valid extensions
-  const computedResult = folders.map(phashCompute);
-  // const computedResult = scaner.map(phashCompute);
+  // -! Use the pHashCompute on each filesPaths with valid extensions
 
-  // Spark the scanner (starting it) login the computed or cached
-  // pHash to the console
+  const computedResult = folders.map(imagePath => phashCompute(boxImageFileWithStats(imagePath)));
+
+  // -! Spark the scanner (starting it) login the computed or cached
 
   for await (const pHash of computedResult) {
-    // console.log('scaned! \n');
+    console.log('loging:', pHash);
     void pHash;
   }
 })();
+
+// const scaner = BoxedGenerator.fromGen<any>(folders.scan);
+// const computedResult = scaner.map(phashCompute);
+// pHash to the console
+// const asyncComputedResult = BoxedAsyncGenerator.fromAsyncGen(folders.map(idem => idem));
+// const computedResult = asyncComputedResult.mapAwait(phashCompute);
+
+export type WithStats1 = {
+  awaited: {
+    stat: Promise<Stats>;
+  };
+};
+
+export type WithAwaited<T extends Object> = {
+  awaited: T;
+};
+
+export type WithStats = WithAwaited<{
+  stat: Promise<Stats>;
+}>;
+
+// function someAwaited() {}
+/*
+type WithStats2 = {
+    awaited: {
+        stat: Promise<Stats>;
+    };
+}
+type WithStats1 = {
+    awaited: {
+        stat: Promise<Stats>;
+    };
+}
+
+ */
