5a0f82062dd38beb8299f499631fede7e5dab66e
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-11-07 13:33:14 -0500

    chore: :wrench: Update config files.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,15 +1,10 @@
 // Documentation for this file: https://prettier.io/en/configuration.html
 module.exports = {
-  // We use a larger print width because Prettier's word-wrapping seems to be tuned
-  // for plain JavaScript without type annotations
-  printWidth: 120,
+  printWidth: 80,
   proseWrap: 'preserve',
-  // Use .gitattributes to manage newlines
-  endOfLine: 'auto',
+  endOfLine: 'lf',
   // Use single quotes instead of double quotes
   singleQuote: true,
-  // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
-  // to use them for arrays only
   trailingComma: 'es5',
   insertPragma: false,
   requirePragma: false,
