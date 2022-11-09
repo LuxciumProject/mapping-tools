4873a17ef8802136992a2c23ac6a1023e1275022
Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>
2022-11-08 20:13:17 -0500

    chore: :ambulance: Fix build process.
    Signed-off-by: Luxcium (Benjamin Vincent) <luxcium@users.noreply.github.com>

--- common/scripts/project/build.sh
+++ common/scripts/project/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tsc --noEmit false --generateCpuProfile ./out/performance/compile-task.cpuprofile || exit 1
+tsc --noEmit false --generateCpuProfile ./out/performance/out.cpuprofile || exit 1
