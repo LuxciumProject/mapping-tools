14f70d5b63fec6ab9f1b1002e805d46121fd00d4
Benjamin Vincent Kasapoglu <luxcium@users.noreply.github.com>
2022-12-07 01:32:56 -0500

    feat(library): 🏷️ Add types for the collections.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@users.noreply.github.com>

--- library/mapping-tools/src/types/Collection.ts
+++ library/mapping-tools/src/types/Collection.ts
@@ -1,3 +1,78 @@
-import { Settled } from '.';
+import { Settled, SettledLeft, SettledRight } from '.';
 
-export type Collection<T> = Iterable<T | Settled<T> | PromiseSettledResult<T>>;
+// Iterable<PromiseLike<Settled<T>>>
+// PromiseLike<Iterable<Settled<T>>>
+
+// collection: Collection<T> | PromiseLike<Collection<T>>,
+
+export type Collection<U> = Iterable<U | Settled<U> | PromiseSettledResult<U>>;
+
+/*
+Collection<T> | PromiseLike<Collection<T>> | Iterable<PromiseLike<Settled<T>>>
+
+export type MyLongVersionCatchAllType =
+| PromiseLike<T | SettledLeft | SettledRight<T>| PromiseFulfilledResult<T> | PromiseRejectedResult>
+| Iterable<T | SettledLeft | SettledRight<T>| PromiseFulfilledResult<T> | PromiseRejectedResult>
+| PromiseLike<Iterable<T | SettledLeft | SettledRight<T>| PromiseFulfilledResult<T> | PromiseRejectedResult>>;
+| Iterable<PromiseLike<T | SettledLeft | SettledRight<T>| PromiseFulfilledResult<T> | PromiseRejectedResult>>;
+
+Settled<T> PromiseSettledResult<T>
+ */
+
+type NamingIsNotAlwaysTheEasiestThingToDoInTheWorld<T> =
+  | T
+  | Settled<T>
+  | PromiseSettledResult<T>;
+type Alias<T> = NamingIsNotAlwaysTheEasiestThingToDoInTheWorld<T>;
+
+type AliasOrPromiseAlias<T> = Alias<T> | PromiseLike<Alias<T>>;
+export type MyLongVersionCatchAllType<T> =
+  | Alias<T>
+  | Iterable<AliasOrPromiseAlias<T>>
+  | PromiseLike<Alias<T> | Iterable<AliasOrPromiseAlias<T>>>;
+
+type TheOtherWayAround<A> = A | Iterable<A> | PromiseLike<A>;
+type Level2<B> = TheOtherWayAround<TheOtherWayAround<B>>;
+export type Level3<C> = Level2<C | Settled<C> | PromiseSettledResult<C>>;
+
+export type MyLongVersionCatchAllType_Full<T> =
+  | T
+  | SettledLeft
+  | SettledRight<T>
+  | PromiseFulfilledResult<T>
+  | PromiseRejectedResult
+  | Iterable<
+      | T
+      | SettledLeft
+      | SettledRight<T>
+      | PromiseFulfilledResult<T>
+      | PromiseRejectedResult
+      | PromiseLike<
+          | T
+          | SettledLeft
+          | SettledRight<T>
+          | PromiseFulfilledResult<T>
+          | PromiseRejectedResult
+        >
+    >
+  | PromiseLike<
+      | T
+      | SettledLeft
+      | SettledRight<T>
+      | PromiseFulfilledResult<T>
+      | PromiseRejectedResult
+      | Iterable<
+          | T
+          | SettledLeft
+          | SettledRight<T>
+          | PromiseFulfilledResult<T>
+          | PromiseRejectedResult
+          | PromiseLike<
+              | T
+              | SettledLeft
+              | SettledRight<T>
+              | PromiseFulfilledResult<T>
+              | PromiseRejectedResult
+            >
+        >
+    >;
