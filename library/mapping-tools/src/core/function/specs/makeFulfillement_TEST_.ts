978053b5e976f6f159b5139acde851dde0813383
Benjamin Vincent Kasapoglu <luxcium@users.noreply.github.com>
2022-12-09 22:17:37 -0500

    refactor(library): 🔥 Remove specs and smoke-tests.
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@users.noreply.github.com>

--- library/mapping-tools/src/core/function/specs/makeFulfillement_TEST_.ts
+++
@@ -1,9 +0,0 @@
-import { makeFulfillement } from '../../../functions/core/makeFulfillement';
-
-/* istanbul ignore next */
-export function makeFulfillement_TEST_(counter: { a: number }) {
-  console.log(`at: makeFulfillement_TEST_ from ${__filename}`);
-  console.log(makeFulfillement({ value: 10, index: 0, transformStep: 0 }));
-  counter.a++;
-  return counter;
-}
