e67d4526619e2a106bebf72a4f267bc35ce362b2
Luxcium <42672814+Luxcium@users.noreply.github.com>
2023-05-05 12:56:07 -0400

    (FIX):⚕️ 🛗UPGRADE📤
    Signed-off-by: Luxcium <42672814+Luxcium@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -19,3 +19,13 @@ export const REJECTED: 'rejected' = 'rejected' as const;
  * @public
  */
 export const NULL_SYMBOL: unique symbol = Symbol.for('NULL_SYMBOL');
+
+// 1st, 2nd, 3rd, 4th, 5th, ct, fi, fl, AE, ff, fi, ffi, ffl, ft, ij, ll, ffi, mm, nd, nt, oe, one_half, one_quarter, 1/2 == // www one_third, ord, rd, rs, sf, st, three_quarters, Delta, CO2, H2O, 2¹⁰, H₂SO₄, 3≤4, 5≥2, ±, @©®™
+
+/*
+
+*/
+/*
+"'aalt', 'calt', 'case', 'ccmp', 'cv01', 'cv02', 'cv03', 'cv04', 'cv05', 'cv06', 'cv07', 'cv08', 'cv09', 'cv10', 'cv11', 'cv12', 'cv13', 'cv14', 'cv15', 'cv16', 'cv17', 'cv18', 'cv19', 'dnom', 'frac', 'hwid', 'numr', 'onum', 'ordn', 'salt', 'sinf', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09', 'ss10', 'subs', 'sups', 'tnum', 'zero', 'pnum'"
+ */
+// "editor.fontLigatures": "aalt, calt, case, ccmp, cv01, cv02, cv03, cv04, cv05, cv06, cv07, cv08, cv09, cv10, cv11, cv12, cv13, cv14, cv15, cv16, cv17, cv18, cv19, dnom, frac, hwid, numr, onum, ordn, salt, sinf, ss01, ss02, ss03, ss04, ss05, ss06, ss07, ss08, ss09, ss10, subs, sups, tnum, zero, pnum"
