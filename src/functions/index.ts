c7b0edff4c2aacbf6d6f311089a64b14fe64d9a7
Luxcium <42672814+Luxcium@users.noreply.github.com>
2023-05-26 02:45:09 -0400

    fix: 🐛 Fix bugs.
    Signed-off-by: Luxcium <42672814+Luxcium@users.noreply.github.com>

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,8 +1,8 @@
 /* istanbul ignore file */
 export { awaitedMapping } from './awaitedMapping';
+export { fn_a1f9a } from './core';
 export { generateMapping } from './generateMapping';
 export { generateMappingAsync } from './generateMappingAsync';
 export { parallelMapping } from './parallelMapping';
 export { serialMapping } from './serialMapping';
-
 // TASK LIST: [TODO] (Review Documentation) --------------------------
