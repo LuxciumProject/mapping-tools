d881e2dc8e4c9029021ce1991d028a40a48e2663
Benjamin Vincent <luxcium@neb401.com>
2024-02-10 07:38:59 -0500

    feat: ✨ †Scientia es lux principium✨™ SpecialBox
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

---
+++ src/functions/core/SpecialBox.ts
@@ -0,0 +1,55 @@
+export abstract class Box {
+  // properties and methods go here
+}
+
+// export interface Unboxable<T> {
+//   unbox(): T;
+// }
+
+export type UnboxableFn<T, R> = Unboxable<(input: T) => R>;
+
+// Assuming Unboxable interface is defined as:
+interface Unboxable<T> {
+  unbox(): T;
+}
+
+export class SpecialBox<T, R> implements Unboxable<(input: T) => R> {
+  private readonly _fn: (input: T) => R;
+
+  // Static factory method to create a new SpecialBox with a function
+  static of<T, R>(fn: (input: T) => R): SpecialBox<T, R> {
+    return new SpecialBox(fn);
+  }
+
+  static from<TVal, RVal>(
+    box: UnboxableFn<TVal, RVal>
+  ): SpecialBox<TVal, RVal> {
+    return SpecialBox.of(box.unbox());
+  }
+
+  protected constructor(fn: (input: T) => R) {
+    this._fn = fn;
+  }
+
+  public pipe<U>(nextFn: (input: R) => U): SpecialBox<T, U> {
+    const composedFn = (input: T) => nextFn(this._fn(input));
+    return SpecialBox.of(composedFn);
+  }
+
+  public compose<U>(prevFn: (input: U) => T): SpecialBox<U, R> {
+    const composedFn = (input: U) => this._fn(prevFn(input));
+    return SpecialBox.of(composedFn);
+  }
+
+  public unbox(): (input: T) => R {
+    return this._fn;
+  }
+
+  get fn(): (input: T) => R {
+    return this._fn;
+  }
+
+  get value(): (input: T) => R {
+    return this._fn;
+  }
+}
