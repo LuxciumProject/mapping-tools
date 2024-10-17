700bce00f222ad75c7c84b9bf05fd61f12543b94
Luxcium <Luxcium@users.noreply.github.com>
2024-10-17 19:27:51 -0400

    feat: Add URL information for raw GitHub file access.
    Signed-off-by: Luxcium <Luxcium@users.noreply.github.com>

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -6,3 +6,13 @@ export { generateMappingAsync } from './generateMappingAsync';
 export { parallelMapping } from './parallelMapping';
 export { serialMapping } from './serialMapping';
 // TASK LIST: [TODO] (Review Documentation) --------------------------
+
+// https://raw.githubusercontent.com/LuxciumProject/mapping-tools/feat/chaining/src/functions/index.ts
+
+const userName = 'LuxciumProject'
+const repositoryName = 'mapping-tools'
+const branchName = 'feat/chaining'
+const localPath = 'src/functions'
+const fileName = 'index.ts'
+export const url =
+`https://raw.githubusercontent.com/${userName}/${repositoryName}/${repositoryName}/${branchName}/${localPath}/${fileName}`
